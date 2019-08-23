3b1fbfd11b229230bf0baf6ff25d06e751a703e4
Mike Lofgren <mikeylofgren@gmail.com>
2019-08-23 11:06:42 -0700

    add migration table

---
+++ db/migrate/20190823180419_add_locations_table.rb
@@ -0,0 +1,9 @@
+class AddLocationsTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :locations do |t|
+      t.column :name, :string
+      t.column :place, :string
+      t.column :content, :string
+      t.timestamps()
+  end
+end
