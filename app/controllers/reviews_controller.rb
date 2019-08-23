d67627eb9d38ad5dd3b7a82e229082d167581b9c
Mike Lofgren <mikeylofgren@gmail.com>
2019-08-23 15:42:34 -0700

    finish review controller.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
-class ReviewsContoller < ApplicationController
+class V1::ReviewsController < ApplicationController
+
 
   def new
     @location = Location.find(params[:location_id])
@@ -15,6 +16,33 @@ class ReviewsContoller < ApplicationController
   def show
     @location = Location.find(params[:location_id])
     @review = @location.reviews.find(params[:id])
+    json_response(@review)
   end
-  
+
+  def update
+    @location = Location.find(params[:location_id])
+    @review = @location.reviews.find(params[:id])
+    @review.update!(review_params)
+    if @review.update!(review_params)
+      render status: 200, json: {
+        message: "This review has been updated successfully."
+      }
+    end
+  end
+
+  def destroy
+    @location = Location.find(params[:location_id])
+    @review = @location.reviews.find(params[:id])
+    @review.destroy!
+    if @review.destroy!
+      render status: 200, json: {
+        message: "This review has been destroyed successfully."
+      }
+    end
+  end
+
+  private
+  def review_params
+    params.permit(:author, :rating, :content_body)
+  end
 end
